5c83e3d2725cab5e404facb13b89de38dd2c515f
harsh <contact@seldomlabs.com>
2025-02-23 20:40:07 +0530

    changes

--- matchmaker/src/main/java/com/matchmaker/controller/MatchController.java
+++ matchmaker/src/main/java/com/matchmaker/controller/MatchController.java
@@ -4,9 +4,13 @@ import com.matchmaker.common.dto.MatchInfoAddRequest;
 import com.matchmaker.common.dto.MatchInfoAddResponse;
 import com.matchmaker.common.dto.FindMatchResponse;
 import com.matchmaker.common.dto.FindMatchRequest;
+import com.matchmaker.constants.GlobalConstants;
 import com.matchmaker.constants.RequestURI;
 import com.matchmaker.service.MatchInfoService;
 import com.matchmaker.service.MatchService;
+import com.matchmaker.service.impl.MatchServiceImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +22,8 @@ import javax.validation.Valid;
 @Controller
 public class MatchController {
 
+    Logger logger = LogManager.getLogger(MatchController.class);
+
     @Autowired
     MatchService matchService;
 
@@ -27,6 +33,7 @@ public class MatchController {
     @RequestMapping(method = RequestMethod.GET, value = RequestURI.FIND_MATCH_API)
     @ResponseBody
     ResponseEntity<FindMatchResponse> findMatch(@Valid @RequestBody FindMatchRequest request) throws Exception {
+        logger.info("request is" + GlobalConstants.objectMapper.writeValueAsString(request));
         FindMatchResponse response = matchService.findMatchForUser(request);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
