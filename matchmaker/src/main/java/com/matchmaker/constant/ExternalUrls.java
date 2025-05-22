8ca5a9b95193bc5d23201915554ee4a651654015
harsh <shuklaharsh911@gmail.com>
2025-05-22 18:24:07 +0530

    changed user api path

--- matchmaker/src/main/java/com/matchmaker/constant/ExternalUrls.java
+++ matchmaker/src/main/java/com/matchmaker/constant/ExternalUrls.java
@@ -2,7 +2,7 @@ package com.matchmaker.constant;
 
 public class ExternalUrls {
 
-    public static final String GET_USER_DATA_URL = ExternalHosts.USER_SERVICE_HOST + "users";
+    public static final String GET_USER_DATA_URL = ExternalHosts.USER_SERVICE_HOST + "api/v1/users";
 
     public static final String GET_USER_SUBSCRIPTION_DATA_URL = ExternalHosts.SUBSCRIPTION_SERVICE_HOST + "subscriptions";
 }
